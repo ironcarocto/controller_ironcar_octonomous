7e4a280893438a3573def2d9c8043b038d8267d7
Veltin Dupont <veltind@gmail.com>
2018-01-25 22:35:47 +0100

    extract state from main script.
    No more global variable. Still a ton of mess...

---
+++ ironcar/state.py
@@ -0,0 +1,188 @@
+import os
+
+import numpy as np
+import picamera
+import scipy.misc
+import tensorflow as tf
+from keras.models import load_model
+
+from ironcar_master import cam_resolution
+
+
+class State(object):
+    def __init__(self, state, mode, running, car, save_folder, cam_resolution,
+                 socket, models_path,
+                 curr_direction=0, curr_gas=0, model=None, max_speed_rate=0.5,
+                 fps=60):
+        self.state = state
+        self.mode = mode
+        self.running = running
+        self.n_img = 0
+        self.file_count = 0
+        self.save_folder = save_folder
+        self.cam_resolution = cam_resolution
+        self.fps = fps
+
+        self.car = car
+        self.curr_direction = curr_direction
+        self.curr_gas = curr_gas
+        self.max_speed_rate = max_speed_rate
+
+        self.model = None
+        self.models_path = models_path
+        self.current_model = model
+        self.graph = None
+        self.model_loaded = False if self.current_model is None else True
+
+        self.socket = socket
+        self.mode_function = self.default_call
+
+    def get_gas_from_direction(self, dir):
+        return 0.2
+
+    def default_call(self, img):
+        pass
+
+    def autopilot(self, img):
+        img = np.array([img[80:, :, :]])
+        with self.graph.as_default():
+            pred = self.model.predict(img)
+            print('pred : ', pred)
+            np.save('predictions_log/prediction{}'.format(self.file_count),
+                    pred)
+            self.file_count += 1
+            prediction = list(pred[0])
+        index_class = prediction.index(max(prediction))
+
+        local_dir = -1 + 2 * float(index_class) / float(len(prediction) - 1)
+        local_gas = self.get_gas_from_direction(
+            self.curr_direction) * self.max_speed_rate
+
+        self.car.set_direction(local_dir)
+        if self.state == "started":
+            self.car.set_speed(local_gas)
+        else:
+            self.car.stop()
+
+    def dirauto(self, img):
+        img = np.array([img[80:, :, :]])
+        with self.graph.as_default():
+            pred = self.model.predict(img)
+            print('pred : ', pred)
+
+            prediction = list(pred[0])
+        index_class = prediction.index(max(prediction))
+
+        local_dir = -1 + 2 * float(index_class) / float(len(prediction) - 1)
+        self.car.set_direction(local_dir)
+
+    def training(self, img):
+        image_name = os.path.join(self.save_folder,
+                                  'frame_' + str(self.n_img) + '_gas_' +
+                                  str(self.curr_gas) + '_dir_' + str(
+                                      self.curr_direction) +
+                                  '_' + '.jpg')
+        img_arr = np.array(img[80:, :, :], copy=True)
+        scipy.misc.imsave(image_name, img_arr)
+        self.n_img += 1
+
+    # ------------------- Main camera loop  ---------------------
+    # This function is launched on a separate thread that is supposed to run
+    # permanently to get camera pics
+    def camera_loop(self):
+        cam = picamera.PiCamera(framerate=self.fps)
+        cam.resolution = cam_resolution
+        cam_output = picamera.array.PiRGBArray(cam, size=cam_resolution)
+        stream = cam.capture_continuous(cam_output, format="rgb",
+                                        use_video_port=True)
+
+        for f in stream:
+            img_arr = f.array
+            np.save('images_log/img{}'.format(self.file_count), img_arr)
+            if not self.running:
+                break
+            self.mode_function(img_arr)
+
+            cam_output.truncate(0)
+
+    # ------------------ SocketIO callbacks-----------------------
+    # This will try to load a model when receiving a callback from the node server
+    def on_model_selected(self, model_name):
+        if model_name == self.current_model or model_name == -1: return 0
+        new_model_path = self.models_path + model_name
+        self.socket.emit('msg2user',
+                         'Loading model at path : ' + str(new_model_path))
+        try:
+            self.model = load_model(new_model_path)
+            self.graph = tf.get_default_graph()
+            self.current_model = model_name
+            self.socket.emit('msg2user', ' Model Loaded!')
+            self.model_loaded = True
+            self.on_switch_mode(self.mode)
+        except OSError:
+            self.socket.emit('msg2user',
+                             ' Failed loading model. Please select another one.')
+
+    def on_switch_mode(self, data):
+        # always switch the starter to stopped when switching mode
+        if self.state == "started":
+            self.state = "stopped"
+            self.socket.emit('starter')
+        # Stop the gas before switching mode
+        self.car.stop()
+        self.mode = data
+        if data == "dirauto":
+            self.socket.off('dir')
+            if self.model_loaded:
+                self.mode_function = self.dirauto
+                self.socket.emit('msg2user',
+                                 ' Direction auto mode. Please control the gas using a keyboard or a gamepad.')
+            else:
+                print("model not loaded")
+                self.socket.emit('msg2user', ' Please load a model first')
+        elif data == "auto":
+            self.socket.off('gas')
+            self.socket.off('dir')
+            if self.model_loaded:
+                self.mode_function = self.autopilot
+                self.socket.emit('msg2user',
+                                 ' Autopilot mode. Use the start/stop button to free the gas command.')
+            else:
+                print("model not loaded")
+                self.socket.emit('msg2user', 'Please load a model first')
+        elif data == "training":
+            self.socket.on('gas', self.on_gas)
+            self.socket.on('dir', self.on_dir)
+            self.mode_function = self.training
+            self.socket.emit('msg2user',
+                             ' Training mode. Please use a keyboard or a gamepad for control.')
+        else:
+            self.mode_function = self.default_call
+            self.socket.emit('msg2user', ' Resting')
+        print('switched to mode : ', data)
+        # Make sure we stop even if the previous mode sent a last command before switching.
+        self.car.stop()
+
+    def on_start(self, data):
+        self.state = data
+        print('starter set to  ' + data)
+
+    def on_dir(self, data):
+        self.curr_direction = float(data)
+        if self.curr_direction == 0:
+            self.car.straight_dir()
+        else:
+            self.car.set_direction(self.curr_direction)
+
+    def on_gas(self, data):
+        self.curr_gas = float(data) * self.max_speed_rate
+        print('THIS IS THE CURRENT GAS_COMMAND ', self.curr_gas)
+        if self.curr_gas < 0:
+            self.car.brake()
+        elif self.curr_gas == 0:
+            self.car.stop()
+        else:
+            self.car.set_speed(self.curr_gas)
+
+    def on_max_speed_update(self, new_max_speed):
+        self.max_speed_rate = new_max_speed
--- ironcar_master.py
+++ ironcar_master.py
@@ -1,17 +1,11 @@
-import os
 from threading import Thread
 
-import numpy as np
-import picamera
-import picamera.array
-import scipy.misc
-import tensorflow as tf
-from keras.models import load_model
 from socketIO_client import SocketIO
 
-# *********************************** Parameters ************************************
+# *********************************** Parameters ******************************
 from ironcar.connection import Car
 from ironcar.setup import init_folder
+from ironcar.state import State
 
 models_path = './autopilots/'
 
@@ -23,210 +17,32 @@ commands_json_file = "_commands.json"
 
 file_count = 0
 
-# ***********************************************************************************
+# *****************************************************************************
 
 # --------------------------- SETUP ------------------------
 
 
 save_folder = init_folder()
 
-state, mode, running = "stop", "training", True
-n_img = 0
-curr_direction, curr_gas = 0, 0
-current_model = None
-max_speed_rate = 0.5
-model_loaded = False
-
-# --------------- PWM --------------
-
-
-# ---------------- Different modes functions ----------------
 car = Car()
+socketIO = SocketIO('http://localhost', port=8000, wait_for_connection=False)
 
-
-def get_gas_from_direction(dir):
-    return 0.2
-
-
-def default_call(img):
-    pass
-
-
-def autopilot(img):
-    global model, graph, state, max_speed_rate, file_count
-
-    img = np.array([img[80:, :, :]])
-    with graph.as_default():
-        pred = model.predict(img)
-        print('pred : ', pred)
-        np.save('predictions_log/prediction{}'.format(file_count), pred)
-        file_count += 1
-        prediction = list(pred[0])
-    index_class = prediction.index(max(prediction))
-
-    local_dir = -1 + 2 * float(index_class) / float(len(prediction) - 1)
-    local_gas = get_gas_from_direction(curr_direction) * max_speed_rate
-
-    car.set_direction(local_dir)
-    if state == "started":
-        car.set_speed(local_gas)
-    else:
-        car.stop()
-
-
-def dirauto(img):
-    global model, graph, file_count
-
-    img = np.array([img[80:, :, :]])
-    with graph.as_default():
-        pred = model.predict(img)
-        print('pred : ', pred)
-
-        prediction = list(pred[0])
-    index_class = prediction.index(max(prediction))
-
-    local_dir = -1 + 2 * float(index_class) / float(len(prediction) - 1)
-    car.set_direction(local_dir)
-
-
-def training(img):
-    global n_img, curr_direction, curr_gas
-    image_name = os.path.join(save_folder, 'frame_' + str(n_img) + '_gas_' +
-                              str(curr_gas) + '_dir_' + str(curr_direction) +
-                              '_' + '.jpg')
-    img_arr = np.array(img[80:, :, :], copy=True)
-    scipy.misc.imsave(image_name, img_arr)
-    n_img += 1
-
-
-# ------------------- Main camera loop  ---------------------
-# This function is launched on a separate thread that is supposed to run
-# permanently to get camera pics
-def camera_loop():
-    global state, mode_function, running, file_count
-
-    cam = picamera.PiCamera(framerate=fps)
-    cam.resolution = cam_resolution
-    cam_output = picamera.array.PiRGBArray(cam, size=cam_resolution)
-    stream = cam.capture_continuous(cam_output, format="rgb",
-                                    use_video_port=True)
-
-    for f in stream:
-        img_arr = f.array
-        np.save('images_log/img{}'.format(file_count), img_arr)
-        if not running:
-            break
-        mode_function(img_arr)
-
-        cam_output.truncate(0)
-
-
-# ------------------ SocketIO callbacks-----------------------
-# This will try to load a model when receiving a callback from the node server
-def on_model_selected(model_name):
-    global current_model, models_path, model_loaded, model, graph, mode
-    if model_name == current_model or model_name == -1: return 0
-    new_model_path = models_path + model_name
-    socketIO.emit('msg2user', 'Loading model at path : ' + str(new_model_path))
-    try:
-        model = load_model(new_model_path)
-        graph = tf.get_default_graph()
-        current_model = model_name
-        socketIO.emit('msg2user', ' Model Loaded!')
-        model_loaded = True
-        on_switch_mode(mode)
-    except OSError:
-        socketIO.emit('msg2user',
-                      ' Failed loading model. Please select another one.')
-
-
-def on_switch_mode(data):
-    global mode, state, mode_function, model_loaded, model, graph
-    # always switch the starter to stopped when switching mode
-    if state == "started":
-        state = "stopped"
-        socketIO.emit('starter')
-    # Stop the gas before switching mode
-    car.stop()
-    mode = data
-    if data == "dirauto":
-        socketIO.off('dir')
-        if model_loaded:
-            mode_function = dirauto
-            socketIO.emit('msg2user',
-                          ' Direction auto mode. Please control the gas using a keyboard or a gamepad.')
-        else:
-            print("model not loaded")
-            socketIO.emit('msg2user', ' Please load a model first')
-    elif data == "auto":
-        socketIO.off('gas')
-        socketIO.off('dir')
-        if model_loaded:
-            mode_function = autopilot
-            socketIO.emit('msg2user',
-                          ' Autopilot mode. Use the start/stop button to free the gas command.')
-        else:
-            print("model not loaded")
-            socketIO.emit('msg2user', 'Please load a model first')
-    elif data == "training":
-        socketIO.on('gas', on_gas)
-        socketIO.on('dir', on_dir)
-        mode_function = training
-        socketIO.emit('msg2user',
-                      ' Training mode. Please use a keyboard or a gamepad for control.')
-    else:
-        mode_function = default_call
-        socketIO.emit('msg2user', ' Resting')
-    print('switched to mode : ', data)
-    # Make sure we stop even if the previous mode sent a last command before switching.
-    car.stop()
-
-
-def on_start(data):
-    global state
-    state = data
-    print('starter set to  ' + data)
-
-
-def on_dir(data):
-    global curr_direction
-    curr_direction = float(data)
-    if curr_direction == 0:
-        car.straight_dir()
-    else:
-        car.set_direction(curr_direction)
-
-
-def on_gas(data):
-    global curr_gas, max_speed_rate
-    curr_gas = float(data) * max_speed_rate
-    print('THIS IS THE CURRENT GAS_COMMAND ', curr_gas)
-    if curr_gas < 0:
-        car.brake()
-    elif curr_gas == 0:
-        car.stop()
-    else:
-        car.set_speed(curr_gas)
-
-
-def on_max_speed_update(new_max_speed):
-    global max_speed_rate
-    max_speed_rate = new_max_speed
-
+state = State("started", "training", True, car, save_folder, cam_resolution, socketIO, models_path)
 
 # --------------- Starting server and threads ----------------
-mode_function = default_call
-socketIO = SocketIO('http://localhost', port=8000, wait_for_connection=False)
+mode_function = state.default_call
+
 socketIO.emit('msg2user', 'Starting Camera thread')
-camera_thread = Thread(target=camera_loop, args=())
+camera_thread = Thread(target=state.camera_loop, args=())
 camera_thread.start()
+
 socketIO.emit('msg2user', 'Camera thread started! Please select a mode.')
-socketIO.on('mode_update', on_switch_mode)
-socketIO.on('model_update', on_model_selected)
-socketIO.on('starterUpdate', on_start)
-socketIO.on('maxSpeedUpdate', on_max_speed_update)
-socketIO.on('gas', on_gas)
-socketIO.on('dir', on_dir)
+socketIO.on('mode_update', state.on_switch_mode)
+socketIO.on('model_update', state.on_model_selected)
+socketIO.on('starterUpdate', state.on_start)
+socketIO.on('maxSpeedUpdate', state.on_max_speed_update)
+socketIO.on('gas', state.on_gas)
+socketIO.on('dir', state.on_dir)
 
 try:
     socketIO.wait()

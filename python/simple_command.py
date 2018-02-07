cd45c20791ffa87e91d15a707c276cc58a24f30a
Veltin Dupont <veltind@gmail.com>
2018-02-07 14:18:41 +0100

    add command line arguments

--- python/simple_command.py
+++ python/simple_command.py
@@ -1,18 +1,90 @@
 # Import
 import logging
+from argparse import ArgumentParser
+from os.path import isfile
 
 import Adafruit_PCA9685
 import numpy as np
 import picamera.array
 from keras.models import load_model
 
+DEFAULT_RESOLUTION = 250, 70
+DEFAULT_MODEL_PATH = '/home/pi/ironcar/autopilots/my_autopilot_big.hdf5'
+DEFAULT_SPEED = 420
 
-def init_cam():
-    cam = picamera.PiCamera(resolution=(250, 70), framerate=60)
-    cam_output = picamera.array.PiRGBArray(cam, size=(250, 70))
+
+def main():
+    kwargs = load_args()
+    print(kwargs)
+    run(**kwargs)
+
+
+def load_args():
+    parser = ArgumentParser(description='Control the OCTONOMOUS OCTOCAR.')
+    parser.add_argument('--resolution', dest='resolution', type=int, nargs=2,
+                        default=DEFAULT_RESOLUTION,
+                        help='the (width, height) resolution')
+    parser.add_argument('--model-path', dest='path', type=str, nargs=1,
+                        default=DEFAULT_MODEL_PATH,
+                        help='absolute path to the model')
+    parser.add_argument('--speed', dest='speed', type=float, nargs=1,
+                        default=DEFAULT_SPEED,
+                        help='the car speed (ratio to max speed, from 0 to 1)')
+    args = parser.parse_args()
+    check_valid_args(args)
+    return extract_values(args)
+
+
+def check_valid_args(args):
+    assert isfile(args.path), "Model must exist"
+
+
+def extract_values(args):
+    return {
+        "resolution": tuple(args.resolution),
+        "model_path": args.path,
+        "speed": 400 + 100 * args.speed,
+    }
+
+
+def run(resolution, model_path, speed):
+    # Objects Initialisation
+    # Camera
+    cam, cam_output, stream = init_cam(resolution)
+    # Model
+    model_mlg = load_model(model_path)
+    # Arduino
+    pwm = Adafruit_PCA9685.PCA9685()
+
+    # Start loop
+    cam.start_preview()
+    start_run(stream, pwm, model_mlg, cam_output, speed)
+
+
+def init_cam(resolution=(250, 70)):
+    cam = picamera.PiCamera(resolution=resolution, framerate=60)
+    cam_output = picamera.array.PiRGBArray(cam, size=resolution)
     stream = cam.capture_continuous(cam_output, format='rgb',
                                     use_video_port=True)
     return cam, cam_output, stream
+
+
+def start_run(stream, pwm, model_mlg, cam_output, speed):
+    for pict in stream:
+        try:
+            control_car(pwm, pict, model_mlg, speed)
+            cam_output.truncate(0)
+        except Exception:
+            stop_car(pwm)
+            raise
+
+
+def control_car(pwm, pict, model_mlg, speed):
+    pred = model_mlg.predict(np.array([pict.array]))
+    logging.info(pred)
+    power = command_from_pred(pred)
+    pwm.set_pwm(2, 0, power)
+    pwm.set_pwm(1, 0, speed)
 
 
 def command_from_pred(pred):
@@ -27,42 +99,9 @@ def command_from_pred(pred):
     return power
 
 
-def control_car(cam_output, model_mlg, pict, pwm):
-    pred = model_mlg.predict(np.array([pict.array]))
-    logging.info(pred)
-    power = command_from_pred(pred)
-    pwm.set_pwm(2, 0, power)
-    pwm.set_pwm(1, 0, 420)
-    cam_output.truncate(0)
-
-
 def stop_car(pwm):
     pwm.set_pwm(1, 0, 400)
     pwm.set_pwm(2, 0, 400)
-
-
-def run(cam, cam_output, model_mlg, pwm, stream):
-    cam.start_preview()
-    for pict in stream:
-        try:
-            control_car(cam_output, model_mlg, pict, pwm)
-        except:
-            stop_car(pwm)
-            raise
-
-
-def main():
-    # Objects Initialisation
-    # Camera
-    cam, cam_output, stream = init_cam()
-
-    # Model
-    model_mlg = load_model('/home/pi/ironcar/autopilots/my_autopilot_big.hdf5')
-
-    # Arduino
-    pwm = Adafruit_PCA9685.PCA9685()
-
-    run(cam, cam_output, model_mlg, pwm, stream)
 
 
 if __name__ == '__main__':
